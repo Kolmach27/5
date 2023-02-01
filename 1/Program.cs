472d0262a5d12ed34a56155d1c902b73438fdef1
Kolmagorov Ivan <ivandevastator27@gmail.com>
2023-02-01 17:52:20 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,45 @@
+﻿int Length = InputInt("Введите колличество элементов");
+int[] array = new int[Length];
+Array(array);
+Console.WriteLine("Массив: ");
+PrintArray(array);
+int result = MyArray(array);
+System.Console.WriteLine($"Количество чётных чисел в массиве > {result}");
+
+int InputInt(string message)
+{
+    System.Console.Write(message + " > ");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+int MyArray(int[] array)
+{
+    int count = 0;    
+    for (int i = 0; i  < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+return count;
+}
+void Array(int[] array)
+{
+    for (int i = 0; i  < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i  < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
